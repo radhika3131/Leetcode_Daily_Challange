2107d68e5fd505070351a23e65da0997c3515c47
Radhika Ramsen <102825662+radhika3131@users.noreply.github.com>
2023-11-01 22:20:13 +0530

    Add files via upload

---
+++ CountVowelPermutation.java
@@ -0,0 +1,42 @@
+package com.dataStructures.LeetcodeDailyChallange;
+
+import java.util.Scanner;
+
+public class CountVowelPermutation {
+    public static int countVowelPermutation(int n) {
+        if (n == 0)
+            return 0;
+
+        // dp initialization
+        // f[i][j], string length == i, ending with vowel[j]
+        long[][] f = new long[n][5];
+
+        for (int j = 0; j < 5; j++)
+            f[0][j] = 1;
+
+        int a = 0, e = 1, i = 2, o = 3, u = 4;
+        int mod = (int) Math.pow(10, 9) + 7;
+
+        for (int r = 1; r < n; r++) {
+            f[r][a] = (f[r - 1][e] + f[r - 1][i] + f[r - 1][u]) % mod;
+            f[r][e] = (f[r - 1][a] + f[r - 1][i]) % mod;
+            f[r][i] = (f[r - 1][e] + f[r - 1][o]) % mod;
+            f[r][o] = (f[r - 1][i]) % mod;
+            f[r][u] = (f[r - 1][i] + f[r - 1][o]) % mod;
+        }
+
+        long res = 0;
+        for (int j = 0; j < 5; j++)
+            res = (res + f[n - 1][j]) % mod;
+
+        return (int) res;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = countVowelPermutation(n);
+        System.out.println("output " + result);
+    }
+}
