20e735bcbdf79374ced49d38d97d79e97409ee71
Radhika Ramsen <102825662+radhika3131@users.noreply.github.com>
2023-11-04 23:42:05 +0530

    Add files via upload

---
+++ FindOrgArray.java
@@ -0,0 +1,15 @@
+package com.dataStructures.LeetcodeDailyChallange;
+
+public class FindOrgArray {
+    public int[] findArray(int[] pref) {
+        int[] ans = new int[pref.length];
+
+        ans[0] = pref[0];
+        for (int i = 1; i < ans.length; ++i)
+            ans[i] = pref[i] ^ pref[i - 1];
+
+        return ans;
+    }
+
+
+}
