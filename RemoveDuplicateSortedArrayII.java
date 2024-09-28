9550f1fa911560263fcbaaae18ba25a636a73a96
Radhika Ramsen <102825662+radhika3131@users.noreply.github.com>
2024-09-28 23:56:37 +0530

    Add files via upload

---
+++ RemoveDuplicateSortedArrayII.java
@@ -0,0 +1,58 @@
+package com.dataStructures.TopLeetcodeInterviewQues;
+
+import java.util.Arrays;
+
+public class RemoveDuplicateSortedArrayII {
+    public static int removeDuplicates(int[] nums) {
+        int count = 1;
+        int appear = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] == nums[i - 1]){
+                if(appear == 1){
+                    nums[count] = nums[i];
+                    count++;
+                    appear++;
+                }else if(appear == 2)   continue;
+            }else{
+                nums[count] = nums[i];
+                count++;
+                appear = 1;
+            }
+        }
+        return count;
+    }
+    public static void print(int[] nums, int newLength) {
+        System.out.println("Array after removing duplicates: " + Arrays.toString(nums));
+        System.out.println("New Length: " + newLength);
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        // Test Case 1
+        int[] nums1 = {1, 1, 2, 2, 3, 4, 4, 5};
+        System.out.println("Test Case 1");
+        System.out.println("Original Array: " + Arrays.toString(nums1));
+        int Length1 = removeDuplicates(nums1);
+        print(nums1, Length1);
+
+        // Test Case 2
+        int[] nums2 = {1, 1, 1, 1, 1};
+        System.out.println("Test Case 2");
+        System.out.println("Original Array: " + Arrays.toString(nums2));
+        int Length2 = removeDuplicates(nums2);
+        print(nums2, Length2);
+
+        // Test Case 3
+        int[] nums3 = {};
+        System.out.println("Test Case 3");
+        System.out.println("Original Array: " + Arrays.toString(nums3));
+        int Length3 = removeDuplicates(nums3);
+        print(nums3, Length3);
+
+        // Test Case 4
+        int[] nums4 = {1, 2, 3, 4, 5};
+        System.out.println("Test Case 4");
+        System.out.println("Original Array: " + Arrays.toString(nums4));
+        int Length4 = removeDuplicates(nums4);
+        print(nums4, Length4);
+    }
+}
